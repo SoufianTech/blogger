0b6bd84ccd850ed1e3ba70780b547cf75e460335
SoufianTech <80529519+SoufianTech@users.noreply.github.com>
2021-10-24 18:54:37 +0100

    Update firebaseSetup.js

--- firebaseSetup.js
+++ firebaseSetup.js
@@ -1,15 +1,14 @@
-   // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyBYh4z7rLVuZkg5IvN3oqTNkvBlJ3_Mr6I",
-    authDomain: "soufian-tech-65317.firebaseapp.com",
-    projectId: "soufian-tech-65317",
-    storageBucket: "soufian-tech-65317.appspot.com",
-    messagingSenderId: "461190381375",
-    appId: "1:461190381375:web:5140031d5d28aa4e6f9d8a",
-    measurementId: "G-W40NC4FLN4"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyANIhOc2FMIYGYw029OHYdNj9z01HWQbEs",
+  authDomain: "pavax-f0720.firebaseapp.com",
+  databaseURL: "https://pavax-f0720-default-rtdb.firebaseio.com",
+  projectId: "pavax-f0720",
+  storageBucket: "pavax-f0720.appspot.com",
+  messagingSenderId: "694407956222",
+  appId: "1:694407956222:web:85b93bfa5bc2efd6d947af",
+  measurementId: "G-FRBD6PWCYH"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
